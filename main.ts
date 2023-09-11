52ef4e4bbfcbf91cc1891253b4176d33c8437157
linusbrew <144435495+linusbrew@users.noreply.github.com>
2023-09-11 16:58:16 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,10 +7,12 @@ basic.forever(function () {
     temp = input.temperature() - 5
     ljus = input.lightLevel()
     moist = 0
-    radio.sendString("" + String.fromCharCode(2 ** 16 - 1) + String.fromCharCode(2 ** 16 - 1) + String.fromCharCode(Math.floor(65)))
     basic.showNumber(temp)
     basic.showNumber(ljus)
     basic.showNumber(moist)
+    radio.sendValue("" + String.fromCharCode(2 ** 16 - 1) + "T", temp)
+    radio.sendValue("" + String.fromCharCode(2 ** 16 - 1) + "L", ljus)
+    radio.sendValue("" + String.fromCharCode(2 ** 16 - 1) + "F", moist)
     basic.pause(5000)
     basic.pause(5000)
 })
